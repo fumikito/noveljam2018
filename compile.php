b580c9dc5b93270c872958aa226a671c53916b46
fumikito <guy@hametuha.com>
2018-02-11 16:45:30 +0900

    viewport指定忘れてた

--- compile.php
+++ compile.php
@@ -32,6 +32,7 @@ ob_start();
 <html>
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
 <title><?= $title ?></title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
 <link rel="stylesheet" href="./style.css" />
